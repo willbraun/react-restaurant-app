d2d6c0c0ef1ac9426f088b4344ba06c58d97dc16
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-04 00:35:39 -0400

    syntax update

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,7 @@ const Order = ({state, removeItem, increaseQuantity, decreaseQuantity}) => {
     const orders = state.selection.map(order => <OrderItem key={order.id} {...order} removeItem={removeItem} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity}/>);
     const total = state.selection.reduce((acc, i) => acc + (i.price * i.quantity), 0).toFixed(2);
 
-    const replaceLocaleStorage = array => {
+    const replaceLocaleStorage = (array) => {
         localStorage.clear();
         array.forEach((item, i) => localStorage.setItem(i, JSON.stringify(item)))
     }
