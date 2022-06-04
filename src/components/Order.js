84912fd2f67a7c1b1843935d6dfd697267371d41
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-03 22:33:02 -0400

    added checkout button

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,10 +3,15 @@ import OrderItem from './OrderItem';
 const Order = ({state}) => {
 
     const orders = state.selection.map(order => <OrderItem key={order.id} {...order}/>);
+    const total = state.selection.reduce((acc, i) => acc + i.price, 0).toFixed(2);
 
     return (
         <section className="order">
             {orders}
+            <button className="checkout" type="button">
+                <div>Checkout</div>
+                <div>{`$${total}`}</div>
+            </button>
         </section>
     )
 }
